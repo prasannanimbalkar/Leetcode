b738320af4aad78261d67158ecb6e01025ad238e
Prasanna Nimbalkar <30629172+prasannanimbalkar@users.noreply.github.com>
2022-08-01 01:45:28 -0400

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,25 @@
-var twoSum = function(nums, target) {
+const twoSum = (array, goal) => {
+    let indexes = [];
+
+    for(let i = 0; i < array.length; i++){
+       for(let j = i + 1; j < array.length; j++){
+          if (array[i] + array[j] === goal) {
+            indexes.push(i);
+            indexes.push(j);
+          }
+       }
+    }
+    return indexes;
+}
+
+console.log(twoSum([2,3,4,5,1,2], 8));
+
+
+
+
+
+
+var twoSums = function(nums, target) {
     let map = new Map;
     for (var i = 0; i < nums.length; i++) {
         let complement = target - nums[i];
@@ -8,3 +29,5 @@ var twoSum = function(nums, target) {
         map.set(nums[i], i);
     }
 }
+
+console.log(twoSums([2,3,4,5,1,2], 8));
